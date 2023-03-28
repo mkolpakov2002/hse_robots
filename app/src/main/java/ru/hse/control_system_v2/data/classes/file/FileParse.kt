974efadd960cc135b15455b5fe517c355622700f
Maxim <66460674+mkolpakov2002@users.noreply.github.com>
2023-03-28 19:01:50 +0300

    28.03.2023 -> Исправление в чтении xml файла

--- app/src/main/java/ru/hse/control_system_v2/data/classes/file/FileParse.kt
+++ app/src/main/java/ru/hse/control_system_v2/data/classes/file/FileParse.kt
@@ -2,6 +2,7 @@ package ru.hse.control_system_v2.data.classes.file
 
 import android.net.Uri
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
@@ -26,48 +27,66 @@ class FileParse {
         }
 
         suspend fun parseXml(xml: String) : ProtocolModel{
-
-            try{
             val tagList = ArrayList<XmlTag>()
-            withContext(Dispatchers.IO) {
-                val factory = XmlPullParserFactory.newInstance()
-                factory.isNamespaceAware = true
-                val parser = factory.newPullParser()
-                parser.setInput(xml.reader())
+            try {
+                // Запускаем корутину с диспатчером IO для работы с файлом
+                runBlocking(Dispatchers.IO) {
+                    val factory = XmlPullParserFactory.newInstance()
+                    factory.isNamespaceAware = true
+                    val parser = factory.newPullParser()
+                    parser.setInput(xml.reader())
 
-                var eventType = parser.eventType
-                while (eventType != XmlPullParser.END_DOCUMENT) {
-                    var xmlTag = XmlTag()
-                    when (eventType) {
-                        XmlPullParser.START_TAG -> {
-                            // Обработка начального тега
-                            xmlTag.name = parser.name
-                        }
+                    var eventType = parser.eventType
+                    // Создаем переменную для хранения текущего тега
+                    var currentTag: XmlTag? = null
+                    while (eventType != XmlPullParser.END_DOCUMENT) {
+                        when (eventType) {
+                            XmlPullParser.START_TAG -> {
+                                currentTag = null
+                                // Обработка начального тега
+                                val name = parser.name
+                                // Если имя тега не равно xml (заголовок документа)
+                                if (name != "xml") {
+                                    // Создаем новый тег с именем и пустым значением
+                                    currentTag = XmlTag(name, "")
+                                }
+                            }
 
-                        XmlPullParser.TEXT -> {
-                            // Обработка текста внутри тега
-                            xmlTag.value = parser.text
-                            tagList.add(xmlTag)
-                        }
+                            XmlPullParser.TEXT -> {
+                                /// Получаем текст тега
+                                val text = parser.text
+                                // Если текущий тег не пустой и текст не пустой или пробельный
+                                if (currentTag != null && text.isNotBlank()) {
+                                    // Устанавливаем значение текущего тега равным тексту
+                                    currentTag.value = text
+                                }
+                            }
 
-                        XmlPullParser.END_TAG -> {
-                            // Обработка конечного тега
+                            XmlPullParser.END_TAG -> {
+                                // Обработка конечного тега
+                                // Если текущий тег не пустой
+                                if (currentTag != null) {
+                                    // Добавляем текущий тег в список тегов
+                                    tagList.add(currentTag)
+                                    // Обнуляем текущий тег
+                                    currentTag = null
+                                }
+                            }
                         }
+                        eventType = parser.next()
                     }
-                    eventType = parser.next()
                 }
-            }
-            return ProtocolModel(
-                id = 0,
-                name = "",
-                tagList = tagList
-            )
+                return ProtocolModel(
+                    id = 0,
+                    name = "",
+                    tagList = tagList
+                )
             } catch (e : Exception) {
                 e.printStackTrace()
                 return ProtocolModel(
                     id = 0,
                     name = "",
-                    tagList = ArrayList()
+                    tagList = tagList
                 )
             }
         }
--- app/src/main/java/ru/hse/control_system_v2/ui/protocol/LezhnyovProtocolMenuFragment.kt
+++ app/src/main/java/ru/hse/control_system_v2/ui/protocol/LezhnyovProtocolMenuFragment.kt
@@ -8,17 +8,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ru.hse.control_system_v2.R
+import ru.hse.control_system_v2.data.classes.protocol.ProtocolModel
 
 class LezhnyovProtocolMenuFragment : Fragment() {
 
     // Создаем список тегов для демонстрации
-    private val xmlTags = listOf(
-        XmlTag("title", "Hello world"),
-        XmlTag("author", "John Doe"),
-        XmlTag("date", "2023-03-27")
-    )
+    private val xmlTags = ArrayList<XmlTag>()
     // Создаем адаптер для RecycleView с тегами
-    private val xmlTagAdapter = XmlTagAdapter(xmlTags)
+    private lateinit var xmlTagAdapter: XmlTagAdapter
 
     // Создаем вид RecycleView для фрагмента из макета xml_tag_fragment.xml
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -28,8 +25,11 @@ class LezhnyovProtocolMenuFragment : Fragment() {
     // Инициализируем RecycleView после создания фрагмента
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val protocol =
+            (arguments?.getSerializable("protocol") ?: ProtocolModel(0, "", ArrayList())) as ProtocolModel
         // Находим RecycleView по идентификатору
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        xmlTagAdapter = XmlTagAdapter(protocol.tagList)
         // Устанавливаем адаптер для RecycleView
         recyclerView.adapter = xmlTagAdapter
         // Устанавливаем линейный менеджер для RecycleView
